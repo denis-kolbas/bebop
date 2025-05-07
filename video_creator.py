6712e5e8bced2eb78d71d9a7fbfc1dc63ddfccb6
denis-kolbas <denis.kolbas@gmail.com>
2025-05-07 15:50:56 +0200

    Update video_creator.py

--- video_creator.py
+++ video_creator.py
@@ -14,12 +14,14 @@ def init_gcp():
    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'gcp_credentials.json'
 
 def get_latest_json(bucket_name, folder):
-   storage_client = storage.Client()
-   bucket = storage_client.bucket(bucket_name)
-   blobs = bucket.list_blobs(prefix=folder)
-   latest = max(blobs, key=lambda x: x.updated)
-   json_string = latest.download_as_string()
-   return json.loads(json_string)
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    # Look for json files specifically
+    blobs = list(bucket.list_blobs(prefix=f"{folder}/songs"))
+    if not blobs:
+        raise Exception("No JSON files found")
+    latest = max(blobs, key=lambda x: x.updated)
+    return json.loads(latest.download_as_string())
 
 def create_video(song_data):
    width, height = 1080, 1920
