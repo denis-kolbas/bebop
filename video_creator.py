97638497029332b1d0b98f92846f4ac3499a9c23
denis-kolbas <denis.kolbas@gmail.com>
2025-05-07 17:09:05 +0200

    Update video_creator.py

--- video_creator.py
+++ video_creator.py
@@ -9,6 +9,7 @@ clip = ColorClip(size=(720, 480), color=(255, 128, 0)).with_duration(2)
 # Create text
 txt = TextClip(
     text="Test",
+    font="Arial",
     font_size=70,
     color='white'
 ).with_duration(2).with_position('center')
